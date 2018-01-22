48d801153466b4db954864fe0944510e13364b55
Emily Lam <hlam93@gmail.com>
2018-01-21 18:50:44 -0800

    staircase solution

---
+++ staircase.js
@@ -0,0 +1,12 @@
+var n = 6;
+
+
+for (var i = n - 1; i >= 0; i--) {
+    for (var j = 0; j < i; j++) {
+        process.stdout.write(' ');
+    }
+    for (var k = i; k < n; k++) {
+        process.stdout.write('#');
+    }
+    process.stdout.write("\n");
+}
